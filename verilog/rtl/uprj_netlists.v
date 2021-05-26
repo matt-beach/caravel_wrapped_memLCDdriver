0627688ab915a571cda77a7696a6942dc51a36dd
zerotoasic <zerotoasic@zerotoasic.vm.com>
2021-05-26 16:41:47 +0200

    initial commit

---
+++ verilog/rtl/uprj_netlists.v
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+// Include caravel global defines for the number of the user project IO pads 
+`include "defines.v"
+`define USE_POWER_PINS
+
+`ifdef GL
+    // Assume default net type to be wire because GL netlists don't have the wire definitions
+    `default_nettype wire
+    `include "gl/user_project_wrapper.v"
+    `include "gl/user_proj_example.v"
+`else
+    `include "user_project_wrapper.v"
+    `include "wrapped_memLCDdriver/wrapper.v"
+    `include "wrapped_memLCDdriver/memLCDdriver/src/memLCDdriver.v"
+    `include "wrapped_memLCDdriver/memLCDdriver/src/spi_s.v"
+    `include "wrapped_memLCDdriver/memLCDdriver/src/sfifo.v"
+    `include "wrapped_memLCDdriver/memLCDdriver/src/fifomem.v"
+    `include "wrapped_memLCDdriver/memLCDdriver/src/clockdiv.v"
+    `include "wrapped_memLCDdriver/memLCDdriver/src/memlcd_fsm.v"
+`endif
